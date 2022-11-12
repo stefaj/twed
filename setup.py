76b965732811b06f27d1c807a0573db398371d7d
SJ <stefanjacholke@gmail.com>
2022-11-12 12:23:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 import numpy
 import os
-os.environ['LDFLAGS'] = '-framework Carbon'
+# os.environ['LDFLAGS'] = '-framework Carbon'
 
 # define the extension module
 twed = Extension('twed', sources=['twed.c'],include_dirs=[numpy.get_include()])
