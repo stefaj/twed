41b557b53298219b2519e16309d97b987873464f
SJ <stefanjacholke@gmail.com>
2022-11-12 12:22:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ import os
 os.environ['LDFLAGS'] = '-framework Carbon'
 
 # define the extension module
-twed = Extension('twed', sources=['twed.c'])
+twed = Extension('twed', sources=['twed.c'],include_dirs=[numpy.get_include()])
 
 # run the setup
 setup(ext_modules=[twed])
+
+
